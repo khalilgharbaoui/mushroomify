bd0deb7c305a5f12ffecd3e33bd095a49a42f91e
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-08 09:41:42 +0200

    Add scopes for the attributes to the mushroom model
    TODO: will refactor this later on.
    Most of it came from the rails docs:
    https://api.rubyonrails.org/classes/ActiveRecord/Scoping/Named/ClassMethods.html
    
    Also found this post, great stuff exactly what i was looking for to refresh my memory.
    https://www.justinweiss.com/articles/search-and-filter-rails-models-without-bloating-your-controller

--- app/models/mushroom.rb
+++ app/models/mushroom.rb
@@ -10,4 +10,10 @@ class Mushroom < ApplicationRecord
                     red: 7, white: 8, yellow: 9 }
   # fibrous=f,grooves=g,scaly=y,smooth=s
   enum cap_surface: { fibrous: 0, grooves: 1, scaly: 2, smooth: 3 }
+
+  scope :eadible, -> (eadible) { where eadible: eadible }
+  scope :cap_shape, -> (cap_shape) { where cap_shape: cap_shape }
+  scope :cap_color, -> (cap_color) { where cap_color: cap_color }
+  scope :cap_surface, -> (cap_surface) { where cap_surface: cap_surface }
+  scope :bruiser, -> (bruiser) { where bruiser: bruiser }
 end
