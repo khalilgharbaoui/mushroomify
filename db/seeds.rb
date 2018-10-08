9b8bded4efcc97200b9ee4e488f5ce98f47d3473
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-08 10:50:47 +0200

    Generate some fake data with seeds
    Now the form works as it should, Index is filtrable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+20.times do |i|
+  eadible = %w[eadible poisonous].sample
+  cap_shape = %w[bell conical convex flat knobbed sunken].sample
+  cap_color = %w[brown buff cinnamon gray green pink purple red white yellow].sample
+  cap_surface = %w[fibrous grooves scaly smooth].sample
+  bruiser = %w[bruises no].sample
+  Mushroom.create!(identifier: i, eadible: eadible, cap_shape: cap_shape, cap_color: cap_color, cap_surface: cap_surface, bruiser: bruiser)
+end
