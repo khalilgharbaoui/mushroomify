af9c8f856a9e0990fe9f85b1ce68ee2bd2fb1860
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-08 10:04:55 +0200

    Add filter using the scopes in the controller.
    What this does is assigne all mushrooms to the the @mushrooms
    instance variable, once we chose an attribute in the view and
    add its value to the params, we overwrite the request with those params
    so we end up with a scoped/filter instance variable holding only
    those mushrooms that correspond to the values in the params.
    
    TODO: Again this not Dry yet will refactor

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -5,6 +5,11 @@ class MushroomsController < ApplicationController
   # GET /mushrooms.json
   def index
     @mushrooms = Mushroom.all
+    @mushrooms = @mushrooms.eadible(params[:eadible]) if params[:eadible].present?
+    @mushrooms = @mushrooms.cap_surface(params[:bruiser]) if params[:bruiser].present?
+    @mushrooms = @mushrooms.cap_shape(params[:cap_shape]) if params[:cap_shape].present?
+    @mushrooms = @mushrooms.cap_color(params[:cap_color]) if params[:cap_color].present?
+    @mushrooms = @mushrooms.cap_surface(params[:cap_surface]) if params[:cap_surface].present?
   end
 
   # GET /mushrooms/1
