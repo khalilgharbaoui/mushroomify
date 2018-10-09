b34a71584c44b81a966de6c52cee3c01e72ce50b
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-09 05:09:31 +0200

    Complete the migrations file with the remaining attributes

--- db/migrate/20181008064117_create_mushrooms.rb
+++ db/migrate/20181008064117_create_mushrooms.rb
@@ -4,9 +4,27 @@ class CreateMushrooms < ActiveRecord::Migration[5.2]
       t.string :identifier
       t.integer :edible
       t.integer :cap_shape
-      t.integer :cap_surface
       t.integer :cap_color
+      t.integer :cap_surface
       t.integer :bruiser
+      t.integer :odor
+      t.integer :gill_attachment
+      t.integer :gill_spacing
+      t.integer :gill_size
+      t.integer :gill_color
+      t.integer :stalk_shape
+      t.integer :stalk_root
+      t.integer :stalk_surface_above_ring
+      t.integer :stalk_surface_below_ring
+      t.integer :stalk_color_above_ring
+      t.integer :stalk_color_below_ring
+      t.integer :veil_type
+      t.integer :veil_color
+      t.integer :ring_number
+      t.integer :ring_type
+      t.integer :spore_print_color
+      t.integer :population
+      t.integer :habitat
 
       t.timestamps
     end
